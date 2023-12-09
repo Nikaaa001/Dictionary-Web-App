edc1ac62a78e2592618718f4d7f714adf0d0d443
Nikaaa001 <99095382+Nikaaa001@users.noreply.github.com>
2023-12-09 21:30:17 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function App() {
+
+  const [font, setFont] = useState("Sans Serif");
+  const [burgerMenu, setBurgerMenu] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [phonetic, setPhonetic] = useState("");
+
+
+  const getData = async () => {
+    try {
+
+      const response = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/" + inputValue
+      );
+
+      const firstEntry = response.data[0];
+
+      if (firstEntry && firstEntry.phonetics && firstEntry.phonetics.length > 0) {
+        setPhonetic(firstEntry.phonetics[0].text);
+      }
+      else {
+        setPhonetic(false);
+      }
+      if (firstEntry.phonetics[0].audio) {
+        const audio = new Audio(firstEntry.phonetics[0].audio);
+        audio.play()
+          .then(() => console.log("Audio played successfully"))
+          .catch(error => console.error("Error playing audio:", error));
+          console.log(audio)
+      }
+    } catch (error) {
+        setPhonetic(false);
+    }
+  };
+
+
+
+  // console.log(inputValue);
+  // console.log(burgerMenu);
+
+  return (
+    <>
+      <div className="w-[737px] mx-auto">
+
+        {/* Header */}
+
+        <div className=" w-full h-[37px] flex justify-between items-center mt-[58px]">
+
+          <img src="./assets/images/logo.svg" alt="Logo" />
+          
+          <div className="relative w-[253px] h-[full] flex items-center justify-between">
+
+            <div className="flex items-center hover:cursor-pointer" onClick={() => setBurgerMenu(true)}>
+              <span className='text-18px'>{font}</span>
+              <img src="./assets/images/icon-arrow-down.svg" alt="" className="ml-[16px]" />
+            </div>
+
+            <div className={`absolute w-[183px] h-[152px] rounded-[16px] bg-[#FFF] shadow-fontShadow top-[25px] left-[-70px] flex-col ${burgerMenu === false ? 'hidden' : 'flex'}`}>
+
+              <span className=' hover:cursor-pointer' onClick={() => {
+                setFont("Sans Serif")
+                setBurgerMenu(false)
+                }}>Sans Serif</span>
+
+              <span className=' hover:cursor-pointer' onClick={() => {
+                setFont("Serif")
+                setBurgerMenu(false)
+                }}>Serif</span>
+
+              <span className=' hover:cursor-pointer' onClick={() => {
+                setFont("Mono")
+                setBurgerMenu(false)
+                }}>Mono</span>
+
+            </div>
+
+            <div className="w-[1px] h-[36px] bg-line"></div>
+
+            <div className="w-[80px] flex justify-between">
+
+              <div className="w-[40px] h-[20px] bg-darkBtn rounded-[10px] flex items-center">
+                <div className="w-[14px] h-[14px] bg-[#FFF] rounded-[50%] ml-[3px]"></div>
+              </div>
+
+              <img src="./assets/images/icon-moon.svg" alt="Moon" className="" />
+
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* Header */}
+
+        <div>
+
+          <div className="w-[736px] h-[64px] flex bg-search justify-between items-center pl-[24px] pr-[24px] rounded-[16px] mt-[51px]">
+
+            <input className='bg-search outline-none w-[95%]'
+            type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+
+            <img className='w-[16px] h-[16px]'
+            src="./assets/images/icon-search.svg" alt="" onClick={() => {
+              inputValue !== "" ? getData() : null;
+            }} />
+
+          </div>
+
+          <div className={`w-[100%] h-[204px] mt-[132px] text-center ${phonetic === false ? 'block' : 'hidden'}`}>
+            <p>😕</p>
+
+            <h3 className='text-blackOne mt-[44px]'>No Definitions Found</h3>
+
+            <p className=' text-darkBtn mt-[24px]'>
+              Sorry pal, we couldn't 
+              find definitions for the word you were looking for. 
+              You can try <br></br> the search again at later time or head to the web instead.</p>
+          </div>
+
+          <div className="">
+
+            <p className={phonetic === false ? 'hidden' : 'block'}>{inputValue}</p>
+
+            <p>{phonetic}</p>
+
+          </div>
+        
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'line': '#E9E9E9',
+        'darkBtn': '#757575',
+        'racxanairiPurple': '#A445ED',
+        'search': '#F4F4F4',
+        'blackOne': '#2D2D2D',
+      },
+      boxShadow: {
+        'fontShadow': '0px 5px 30px 0px rgba(0, 0, 0, 0.10)',
+      },
+      fontSize: {
+        '18px': '18px',     
+      }
+    },
+  },
+  plugins: [],
+}
+
